3ea634b98e78e71f964e74506049f5fe2f7b910b
h8k8n <hakan.sengul08@gmail.com>
2024-03-14 01:05:19 +0300

    added tasks

---
+++ src/child/Tasks.jsx
@@ -0,0 +1,11 @@
+/*import { TabView, TabPanel } from "primereact/tabview";*/
+import React, { useState, useEffect } from "react";
+import { Button } from "primereact/button";
+
+export const Tasks = () => {
+  return (
+    <>
+      <p>Tasks</p>
+    </>
+  );
+};
