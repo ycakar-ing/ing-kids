9eafb3e8a0ff61a53386e8a3145839d63f24c228
ycakar <yigit.cakar@bayzat.com>
2024-03-23 09:24:23 +0300

    bind gloals and tasks

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ import { Profile } from './profile/Profile';
 import { Notifications } from './notifications/Notifications';
 import { Login } from './login/Login';
 import { useState } from 'react';
-import { ParentDashboard, ChildDashboard, Dashboard } from './dashboard/Dashboard';
+import { Dashboard } from './dashboard/Dashboard';
+
+import './App.css';
 
 const App = () => {
   const [login, setLogin] = useState(false);
-  const [isParent,setIsParent] = useState(true);
+  const [isParent, setIsParent] = useState(true);
 
   const style = {
     padding: '20px'
@@ -32,8 +34,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Login afterLogin={afterLogin} />} />
           <Route path="/dashboard" element={<Dashboard isParent={isParent} />} />
-          <Route path="/tasks" element={<Tasks />} />
-          <Route path="/goals" element={<Goals />} />
+          <Route path="/tasks" element={<Tasks isParent={isParent} />} />
+          <Route path="/goals" element={<Goals isParent={isParent} />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/notifications" element={<Notifications />} />
         </Routes>
