38d8687452c345754e805ea6dc020ac711d12a0e
ycakar <yigit.cakar@bayzat.com>
2024-03-24 22:11:36 +0300

    profile

--- src/profile/Profile.jsx
+++ src/profile/Profile.jsx
@@ -1,7 +1,16 @@
 import { HEADER_MODE_DEEP } from "../App";
+import child from "../assets/child-profile-body.png"
+import parent from "../assets/parent-profile-body.png"
 
-export const Profile  = ({isParent,setHeaderMode}) => {
-      setHeaderMode(HEADER_MODE_DEEP);
-    return (isParent ? <img src="src\assets\parent-profile-body.png"/> : <img src="src\assets\child-profile-body.png"/> );
+
+export const Profile = ({ isParent, setHeaderMode }) => {
+  setHeaderMode(HEADER_MODE_DEEP);
+  const bgStyle = {
+    width: '100%'
+  }
+  let src = isParent ? parent : child;
+  return (<>
+    <img src={src} style={bgStyle} />
+  </>)
 
 }
