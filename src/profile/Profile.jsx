2a0933b9dda29fd6475dc280cfd8d30f3e942eb8
ycakar <yigit.cakar@bayzat.com>
2024-03-24 12:29:32 +0300

    prod config

--- src/profile/Profile.jsx
+++ src/profile/Profile.jsx
@@ -1,6 +1,6 @@
 import { HEADER_MODE_DEEP } from "../App";
 
-export const Profile  = ({isParent}) => {
+export const Profile  = ({isParent,setHeaderMode}) => {
       setHeaderMode(HEADER_MODE_DEEP);
     return (isParent ? <img src="src\assets\parent-profile-body.png"/> : <img src="src\assets\child-profile-body.png"/> );
 
